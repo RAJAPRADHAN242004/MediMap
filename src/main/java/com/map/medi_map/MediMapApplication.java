cec82471a0326f98e0dc9406366187eeb583646e
RAJA DEMO <pradhanraja780@gmail.com>
2025-07-07 21:00:26 +0530

    git setup.

---
+++ src/main/java/com/map/medi_map/MediMapApplication.java
@@ -0,0 +1,13 @@
+package com.map.medi_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/map/medi_map/MediMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.medi_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
